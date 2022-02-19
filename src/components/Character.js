561fd6873fc743d0462293903545eeb80c1a83cf
Ethan Miles <ethanmiles@Randalls-MacBook-Air-2.local>
2022-02-18 23:38:47 -0600

    receiving data from the API

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,17 @@
 // Write your Character component here
+import react, {useState, styledComponents} from 'react';
+
+export default function Character (props) {
+    const { characters } = props
+    console.log('data:', props.characters[0])
+    return (
+        <>
+            <div className='character-wrapper'>
+                <div className='character-list'>
+                </div>
+            </div>
+        </>
+
+
+    )
+}
