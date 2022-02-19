886d4c33018206c246612cee94130fee85ee6853
Ethan Miles <ethanmiles@Randalls-MacBook-Air-2.local>
2022-02-19 12:17:39 -0600

    page now recieves details that can be hidden and shown at the press of a button

--- src/components/Character.js
+++ src/components/Character.js
@@ -2,8 +2,14 @@
 import react, {useState, styledComponents} from 'react';
 
 export default function Character (props) {
-    const { characters } = props
-    console.log('data:', characters)
+    const { characters } = props;
+    const [showDetails, setShowDetails] = useState(null);
+    console.log('data:', characters);
+    
+    const toggleDetails = () => {
+        setShowDetails(!showDetails);
+    }
+
     
     return (
         <>
@@ -11,9 +17,23 @@ export default function Character (props) {
                 <div className='character-list'>
                     {
                         characters.map(character => {
-                            return <h2>{character.name}</h2>
+                            return <div className='character-card'>
+                                <h2>{character.name}</h2>
+                                <button onClick={toggleDetails}>Show More</button>
+                                <div className='details-container'>
+                                {showDetails &&
+                                    <ul id='details'>
+                                        <li>{character.birth_year}</li>
+                                        <li>{character.eye_color}</li>
+                                        <li>{character.gender}</li>
+                                        <li>{character.hair_color}</li>
+                                        <li>{character.height}</li>
+                                        <li>{character.mass}</li>
+                                    </ul>
+                        }
+                                </div>
+                                </div>
                         })
-
                     }
                 </div>
             </div>
