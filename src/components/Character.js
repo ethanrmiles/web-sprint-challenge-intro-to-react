1b7563c716a6329f4edf5f64059e7ca35f59a3c3
Ethan Miles <ethanmiles@Randalls-MacBook-Air-2.local>
2022-02-18 23:44:43 -0600

    DOM now displays A list of characters

--- src/components/Character.js
+++ src/components/Character.js
@@ -3,11 +3,18 @@ import react, {useState, styledComponents} from 'react';
 
 export default function Character (props) {
     const { characters } = props
-    console.log('data:', props.characters[0])
+    console.log('data:', characters)
+    
     return (
         <>
             <div className='character-wrapper'>
                 <div className='character-list'>
+                    {
+                        characters.map(character => {
+                            return <h2>{character.name}</h2>
+                        })
+
+                    }
                 </div>
             </div>
         </>
